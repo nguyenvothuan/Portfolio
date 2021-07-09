70d486a99fdfb225cfb0c22c4d9dd1466b41259b
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2021-07-09 09:20:51 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Portfolio/' // Thay tên repository của các bạn vào đây nhé
+    : '/'
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
